555e7c98b6ff67bf61360f558ec1a08b7dffed77
omar378501 <oma378501@gmail.com>
2015-08-25 18:35:35 -0400

    custom digital clock with buttons: pause, play

--- clock.js
+++ clock.js
@@ -1,14 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  setInterval(function(){
-    var dateTime = new Date();
-    // These 3 variables add a leading '0' to keep the date properly formatted.
-    var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
-    var secondsTwoDigitsWithLeadingZero = ("0" + dateTime.getSeconds()).substr(-2);
-    var millisecondsTwoDigitsWithLeadingZero = ("0" + dateTime.getMilliseconds()).substr(-3);
-    $("#milliseconds").text(millisecondsTwoDigitsWithLeadingZero);
-    $("#seconds").text(secondsTwoDigitsWithLeadingZero);
-    $("#minutes").text(minutesTwoDigitsWithLeadingZero);
-    $("#hours").text(dateTime.getHours());
-  },100);
+    setInterval(function () {
+        // These 3 variables add a leading '0' to keep the date properly formatted.
+        var dateTime = new Date();
+        if (!flag) {
+           
+            var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
+            var secondsTwoDigitsWithLeadingZero = ("0" + dateTime.getSeconds()).substr(-2);
+            var millisecondsTwoDigitsWithLeadingZero = ("0" + dateTime.getMilliseconds()).substr(-3);
+
+            $("#milliseconds").text(millisecondsTwoDigitsWithLeadingZero);
+            $("#seconds").text(secondsTwoDigitsWithLeadingZero);
+            $("#minutes").text(minutesTwoDigitsWithLeadingZero);
+            $("#hours").text(dateTime.getHours());
+        }
+    }, (flag)?0:100);
 });
