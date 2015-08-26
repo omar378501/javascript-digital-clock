3e93ccaff2e0605e1b0c233d46e53ce87b243517
omar378501 <oma378501@gmail.com>
2015-08-25 20:33:22 -0400

    resolv issue wait time to pause

--- clock.js
+++ clock.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
 
-    setInterval(function () {
-        // These 3 variables add a leading '0' to keep the date properly formatted.
-        var dateTime = new Date();
-        if (!flag) {
-           
-            var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
-            var secondsTwoDigitsWithLeadingZero = ("0" + dateTime.getSeconds()).substr(-2);
-            var millisecondsTwoDigitsWithLeadingZero = ("0" + dateTime.getMilliseconds()).substr(-3);
+    if (!flag_pause || flag_play) {
+        setInterval(function () {
+            // These 3 variables add a leading '0' to keep the date properly formatted.
 
-            $("#milliseconds").text(millisecondsTwoDigitsWithLeadingZero);
-            $("#seconds").text(secondsTwoDigitsWithLeadingZero);
-            $("#minutes").text(minutesTwoDigitsWithLeadingZero);
-            $("#hours").text(dateTime.getHours());
-        }
-    }, (flag)?0:100);
+            if (!flag_pause || flag_play) {
+                var dateTime = new Date();
+                var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
+                var secondsTwoDigitsWithLeadingZero = ("0" + dateTime.getSeconds()).substr(-2);
+                var millisecondsTwoDigitsWithLeadingZero = ("0" + dateTime.getMilliseconds()).substr(-3);
+
+                $("#milliseconds").text(millisecondsTwoDigitsWithLeadingZero);
+                $("#seconds").text(secondsTwoDigitsWithLeadingZero);
+                $("#minutes").text(minutesTwoDigitsWithLeadingZero);
+                $("#hours").text(dateTime.getHours());
+            }
+
+        }, 100);
+    }
+
 });
